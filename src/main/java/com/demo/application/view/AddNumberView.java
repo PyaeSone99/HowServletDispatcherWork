f1d01cf2eae04149176b104e22db568fb6787796
Pyae Sone Aung <pyaesoneaungbb99@gmail.com>
2023-03-23 21:15:01 +0630

    servlet dispatcher framework

---
+++ src/main/java/com/demo/application/view/AddNumberView.java
@@ -0,0 +1,13 @@
+package com.demo.application.view;
+
+import com.demo.framework.model.Model;
+import com.demo.framework.view.View;
+
+public class AddNumberView implements View {
+    @Override
+    public String render(Model model) {
+        int num1 = Integer.parseInt(model.get("num1"));
+        int num2 = Integer.parseInt(model.get("num2"));
+        return String.format("%d add %d is :: %d",num1,num2,num1+num2);
+    }
+}
---
+++ src/main/java/com/demo/application/view/IndexView.java
@@ -0,0 +1,17 @@
+package com.demo.application.view;
+
+import com.demo.framework.model.Model;
+import com.demo.framework.view.View;
+
+public class IndexView implements View {
+    @Override
+    public String render(Model model) {
+        return """
+                <h1>Hello from index view</h1>
+                <ul>
+                    <li><a href='/add-number?num1=5&num2=7'>/add-number?num1=5&num2=7</a></li>
+                    <li><a href='/square-number?num1=5'>/square-number?num1=5</a></li>
+                </ul>
+                """;
+    }
+}
